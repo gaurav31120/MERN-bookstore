1dfee5759dc1c58380c62059472bceee36b20bcb
Gaurav Kumar <gaurav31120@gmail.com>
2024-12-01 18:26:24 +0530

    12 creating the router dom for routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,17 @@
 
 import Course from "./components/Course";
 import Home from "./home/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Home />
-    <Course />
-      
+      {/* <Home />
+    <Course /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Course />} />
+      </Routes>
     </>
   );
 }
