333ec12b6515b25e5840fc2716f1f3100101ce29
Gaurav Kumar <gaurav31120@gmail.com>
2024-12-01 16:25:12 +0530

    5 creating the footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@
 
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Footer />
     </>
   );
 }
