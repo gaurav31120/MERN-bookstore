db237acf6bc9577665483bb9bc85bf168abaf620
Gaurav Kumar <gaurav31120@gmail.com>
2024-12-03 07:44:52 +0530

    16 creating the login form ui

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 // import React from "react";
 
 import { useEffect, useState } from "react";
+import Login from "./Login";
 
 function Navbar() {
   const [theme, setTheme] = useState(
@@ -145,9 +146,13 @@ function Navbar() {
               </label>
             </div>
             <div className="">
-              <a className="bg-black text-white px-3 py-2 rounded-md hoverLbg-slate-800 duration-300 cursor-pointer">
+              <a className="bg-black text-white px-3 py-2 rounded-md hoverLbg-slate-800 duration-300 cursor-pointer"
+              onClick={() => {
+                document.getElementById("my_modal_3").showModal()
+              }}>
                 Login
               </a>
+              <Login />
             </div>
           </div>
         </div>
